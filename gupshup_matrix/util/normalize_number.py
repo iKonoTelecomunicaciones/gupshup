ddf3cf2805e44e158174e7c7144a84c0f2b0e5dd
Alejandro Herrera <bramendev@gmail.com>
2022-08-03 14:24:42 -0500

    In the pm it is not obligatory to put + to the telephones

--- gupshup_matrix/util/normalize_number.py
+++ gupshup_matrix/util/normalize_number.py
@@ -3,6 +3,7 @@ remove_extra_chars = str.maketrans("", "", " .,-()")
 
 def normalize_number(number: str) -> str:
     phone = number.translate(remove_extra_chars)
-    if not number.startswith("+") or not phone[1:].isdecimal():
+    number = number if number.startswith("+") else f"+{number}"
+    if not phone[1:].isdecimal():
         raise Exception("Phone number must be entered in international format")
     return phone
