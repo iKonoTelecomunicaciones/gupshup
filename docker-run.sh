107e478d3a4d3ba032ece9561c96099bafd6eaa5
Alejandro Herrera <bramendev@gmail.com>
2022-08-03 13:08:14 -0500

    Fixed building and compiling docker images and logs

--- docker-run.sh
+++ docker-run.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
+cd /opt/gupshup-matrix
 
 # Define functions.
 function fixperms {
-	chown -R $UID:$GID /data /opt/gupshup-matrix
+	chown -R $UID:$GID /data
+
+	# /opt/gupshup-matrix is read-only, so disable file logging if it's pointing there.
+	if [[ "$(yq e '.logging.handlers.file.filename' /data/config.yaml)" == "./gupshup-matrix.log" ]]; then
+		yq -I4 e -i 'del(.logging.root.handlers[] | select(. == "file"))' /data/config.yaml
+		yq -I4 e -i 'del(.logging.handlers.file)' /data/config.yaml
+	fi
 }
 
-cd /opt/gupshup-matrix
 
 if [ ! -f /data/config.yaml ]; then
-	cp gupshup_matrix/example-config.yaml /data/config.yaml
+	cp example-config.yaml /data/config.yaml
 	echo "Didn't find a config file."
 	echo "Copied default config file to /data/config.yaml"
 	echo "Modify that config file to your liking."
