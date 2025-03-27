78aabc4d0bd07009de6f4a03d671e6641619cd9e
@jcardenas4773819 <jcardenas@ikono.com.co>
2025-03-27 17:34:49 -0500

    style: :art: format code to pass tests

--- gupshup_matrix/portal.py
+++ gupshup_matrix/portal.py
@@ -5,8 +5,8 @@ from datetime import datetime
 from string import Template
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union, cast
 
-from aiohttp import ClientConnectorError
 import asyncpg
+from aiohttp import ClientConnectorError
 from markdown import markdown
 from mautrix.appservice import AppService, IntentAPI
 from mautrix.bridge import BasePortal
